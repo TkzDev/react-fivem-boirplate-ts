1f1e0f2119f40dd311a2ffc8a5400a37260e94b5
Rubens Borges <contact@rubensborges.dev>
2025-02-18 12:43:57 -0300

    merge

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { useVisibility, VisibilityProvider } from '@app/contexts/VisibilityContext';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import {
+  useVisibility,
+  VisibilityProvider,
+} from '@app/contexts/VisibilityContext';
 import { useNuiCallback } from '@app/hooks/useNuiCallback';
 import { ColorVariants, attributeColorsToHTML } from '@app/utils/colors';
-import { Home } from './views/pages/home';
 
 export function App() {
   const { visible } = useVisibility();
@@ -24,10 +26,7 @@ export function App() {
   return (
     <VisibilityProvider>
       <div className="w-screen h-screen flex justify-center items-center absolute left-0 top-0 z-[2]">
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="*" element={<Navigate to="/home" />} />
-        </Routes>
+        <Routes></Routes>
       </div>
     </VisibilityProvider>
   );
